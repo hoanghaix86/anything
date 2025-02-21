8f62a4ec71ad59808902e22d5482d468265011c0
HoangHaix86 <hai.hoang.2762@gmail.com>
2025-02-22 02:38:47 +0700

    chore: add type

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 3000)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
+    app.setGlobalPrefix('api')
+    await app.listen(3000)
 }
-bootstrap()
+
+bootstrap().catch((err) => {
+    console.error(`Application bootstrap failed: ${err}`)
+    process.exit(1)
+})
