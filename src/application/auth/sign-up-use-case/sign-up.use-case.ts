4839076e3732eb295d37bc49c17965800094bf5a
HoangHaix86 <hai.hoang.2762@gmail.com>
2025-02-23 00:43:49 +0700

    reactor: use usecase for this

--- src/application/auth/sign-up-use-case/sign-up.use-case.ts
+++ src/application/auth/sign-up-use-case/sign-up.use-case.ts
@@ -1,35 +1,12 @@
-import { Injectable, ConflictException, Inject } from '@nestjs/common'
-import { Account } from 'src/domain/entities/account'
-import { IPasswordHasherPort } from 'src/domain/port/password-hasher.port'
-import { IAccountRepositoryPort } from 'src/domain/port/repositories/account.repository'
-import { EmailValueObject } from 'src/domain/value-objects/email.value-object'
-import { PasswordValueObject } from 'src/domain/value-objects/password.value-object'
+import { Injectable } from '@nestjs/common'
+import { CreateAccountUseCase } from 'src/application/account/create-account-use-case/create-account.use-case'
 import { SignUpCommand } from './sign-up.command'
 
 @Injectable()
 export class SignUpUseCase {
-    constructor(
-        @Inject(IAccountRepositoryPort)
-        private accountRepository: IAccountRepositoryPort,
-        @Inject(IPasswordHasherPort)
-        private passwordHasher: IPasswordHasherPort,
-    ) {}
+    constructor(private readonly createAccountUseCase: CreateAccountUseCase) {}
 
     async execute(input: SignUpCommand): Promise<void> {
-        // Create and validate email value object
-        const email = EmailValueObject.fromRaw(input.email)
-
-        // Check if email already exists
-        const existingAccount = await this.accountRepository.findOneByEmail(email.getValue())
-        if (existingAccount) {
-            throw new ConflictException('Email is already in use')
-        }
-
-        // Create and validate password value object
-        const password = await PasswordValueObject.createFromRaw(input.password, this.passwordHasher)
-
-        // Create and save account
-        const account = Account.create({ email, password })
-        await this.accountRepository.save(account)
+        await this.createAccountUseCase.execute(input)
     }
 }
